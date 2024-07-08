7c7d164238a3e211faa94f06b262f64f470420b3
abhinavkumaryadav <abhinavk1608@gmail.com>
2024-07-08 17:44:04 +0530

    stringbasic

---
+++ Stringbasic.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+
+{
+
+char strex[10]={'a','b','h','i','n','a','v'};
+
+for ( int i = 0; i <10; i++)
+{
+    cout<<strex[i];
+}
+
+
+return 0;
+}
