8c958e5cd1fec3ff895cbf2902c57b6b5a79c2de
abhinavkumaryadav <abhinavk1608@gmail.com>
2024-07-13 16:48:48 +0530

    NumberStarPattern Program VERY IMPORTANT

---
+++ NumberStarPattern-V-IMP.cpp
@@ -0,0 +1,63 @@
+/*
+print the following pattern:-
+input=5
+1 2 3 4 5 5 4 3 2 1
+1 2 3 4 * * 4 3 2 1
+1 2 3 * * * * 3 2 1
+1 2 * * * * * * 2 1
+1 * * * * * * * * 1
+*/
+
+
+
+
+
+
+
+#include<iostream>
+using namespace std;
+
+int main() 
+
+{
+
+int i;
+cout<<"enter a number to print number star pattern"<<endl;
+cin>>i;
+int j=1,n;
+
+while (j<=i)
+{
+    n=1;
+while (n<=i-j+1)
+{
+    cout<<n<<" ";
+    ++n;
+}
+
+n=j;
+
+while (n>0)
+{
+    if (n>1)
+    {
+    cout<<"* *"<<" ";
+    }
+--n;
+}
+n=i-j+1;
+while (n>0)
+{
+    cout<<n<<" ";
+    --n;
+}
+cout<<endl;
+++j;
+
+
+}
+
+
+
+return 0;
+}
