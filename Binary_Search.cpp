f93460f1bf16ff8dbaacdce211aeea13a5c64d4c
abhinavkumaryadav <abhinavk1608@gmail.com>
2024-07-29 00:13:26 +0530

    binary search

---
+++ Binary_Search.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+int BinarySearch(int arr[], int size, int key)
+{
+    int start = 0;
+    int end=size-1;
+    int mid = start +(end-start)/2;
+    while(start <= end)
+    {
+        if ( arr[mid] == key)
+        {
+            cout<< mid;
+        }
+
+        if(key < arr[mid])
+        {
+            end=mid-1;
+        }
+
+        else
+        {
+            start=mid+1;
+        }
+        
+        mid = start +(end-start)/2;
+    }
+    return -1;
+}
+
+
+
+
+
+
+int main() 
+
+{
+
+
+int demo_array[5]={1,2,3,4,5};
+
+cout<<"the index at which the key is present is ";
+BinarySearch(demo_array,5,2);
+
+return 0;
+}
