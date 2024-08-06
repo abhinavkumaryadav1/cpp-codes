754cd9f205eb5b14dbf69a868b824f463812e75a
abhinavkumaryadav <abhinavk1608@gmail.com>
2024-08-06 14:50:40 +0530

    finally coded this UNIQUE OCCURENCE problem

--- Array_UniqueOccurence.cpp
+++ Array_UniqueOccurence.cpp
@@ -1,55 +1,55 @@
-//leetcode not done
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+    
+    bool UniqueOccurrences(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        vector<int> v;
+
+        for (int i = 0; i < arr.size(); i++) {
+            int cnt = 1;
+
+            // Count occurrences of the current element
+            while (i + 1 < arr.size() && arr[i] == arr[i + 1]) 
+            {
+                cnt++;
+                i++;
+            }
+
+            v.push_back(cnt);
+        }
+
+        sort(v.begin(), v.end());
+
+        for (int i = 1; i < v.size(); i++) {
+            if (v[i] == v[i - 1]) {
+               return false;
+                
+            }
+        }
+
+       return true;
+    
+    }
 
 
-// #include<iostream>
-// using namespace std;
+int main()
+{
 
-// int ArrayUniqueOccurence(int arr[],size){
+vector<int> demo={1,2,1,2,2,3,3,3,3,4};
 
-// for (int j = 0; j < size; j++)
-// {
-//     int p=j;
-//     int count=1;
-//     for (int  i = j+1; i < size; i++)
-//     {
-//         if (arr[i]^arr[j]==0)
-//         {
-//             swap(arr[i],arr[j+1]);
-//             count=count+1;
-//             j=j+1;
-//         }
-        
-//     }
-//     j=j+count;
+cout<<"it is ";
+if(UniqueOccurrences(demo)==1)
 
-// }
+    cout<<"true";
+     
+    else {
+    cout<<"false";
+    }
+    cout<<" that the array is unique";
 
 
-
-// }
-
-
-
-
-
-
-
-
-// int main() 
-
-// {
-
-// int s;
-// cout<<"enter the size of the array"<<endl;
-// cin>>s;
-// cout<<"enter the elements of the array";
-// int demo_array[s];
-// for (int  i = 0; i < s; i++)
-// {
-//     cin>>arr[i];
-// }
-
-// ArrayUniqueOccurence(demo_array,s);
-
-// return 0;
-// } 
+    return 0;
+}
