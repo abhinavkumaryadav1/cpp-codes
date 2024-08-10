a5d7134ee628498dc19bb64d319c38114e347424
abhinavkumaryadav <abhinavk1608@gmail.com>
2024-08-10 23:36:35 +0530

    Pointers-Double Pointer property(2)

---
+++ Pointer_DoublePointer.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+
+{
+
+int i=5;
+int * ptr1=&i;
+int ** ptr2=&ptr1;
+
+cout<<" value in i "<<i<<endl;
+cout<<"address of i "<<&i<<endl;
+cout<<"value in ptr1 "<<ptr1<<endl;
+cout<<"value corresponding to address stored in ptr1 "<<*ptr1<<endl;
+cout<<"address of ptr1 "<<&ptr1<<endl;
+cout<<" value is ptr2 "<<ptr2<<endl;
+cout<<" value corresponding to address in ptr2 "<<*ptr2<<endl;
+cout<<"the value corresponding to the address that is a value to a diffrent correspoding address that is stored in ptr2 "<<**ptr2<<endl;
+
+return 0;
+}
