e2421d4763de3ef84b4addbaaedd8947d159b88b
Abhinav Kumar Yadav <abhinavk1608@gmail.com>
2025-08-06 00:08:34 +0530

    Leetcode Flush

---
+++ Array_Leetcode_Find_MissingNo_268.cpp
@@ -0,0 +1,51 @@
+/*268. Missing Number
+
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,0,1]
+
+Output: 2
+
+Explanation:
+
+n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+Example 2:
+
+Input: nums = [0,1]
+
+Output: 2
+
+Explanation:
+
+n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums. */
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+      int max =0;
+      int n= nums.size();
+      
+      for(int i=1;i<=n;i++)
+      {
+        max=max+i;
+        cout<<max<<endl;
+      }
+      cout<<max<<endl;
+  
+      for(int i=0;i<n;i++)
+      {
+        max=max-nums[i];
+      }
+
+      return max;
+
+      
+
+    }
+};
