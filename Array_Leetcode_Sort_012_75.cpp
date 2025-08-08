7dabd32ae7cdf0805df7d4a237ce24e79a90d67c
Abhinav Kumar Yadav <abhinavk1608@gmail.com>
2025-08-09 00:11:21 +0530

    Leetcode Flush

--- Array_Leetcode_Sort_012_75.cpp
+++ Array_Leetcode_Sort_012_75.cpp
@@ -37,7 +37,36 @@ public:
       //   }
       // }
 
-      
+      //approch 2 optimal
+    // Dutch national flag algorithm 3 pointers
+    int i =0;
+    int j =0;
+    int k=nums.size()-1;
+
+    while(j<=k)
+    {
+      if(nums[j]==0)
+      {
+        swap(nums[i],nums[j]);
+        i++;
+        j++;
+      }
+
+      else if(nums[j]==1)
+      {
+        j++;
+      }
+
+      else
+      {
+        swap(nums[j],nums[k]);
+        k--;
+      }
+    }
+
+    
+
+
 
     }
 };
