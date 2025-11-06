1d917609b595a848d0554821f0035637b9d85289
Abhinav Kumar Yadav <abhinavk1608@gmail.com>
2025-11-06 11:43:39 +0530

    Leetcode Flush

---
+++ String_Leetcode_Isomonphic_String_205.cpp
@@ -0,0 +1,69 @@
+/*205. Isomorphic String
+
+there is not Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+Example 1:
+
+Input: s = "egg", t = "add"
+
+Output: true
+
+Explanation:
+
+The strings s and t can be made identical by:
+
+Mapping 'e' to 'a'.
+Mapping 'g' to 'd'.
+Example 2:
+
+Input: s = "foo", t = "bar"
+
+Output: false
+
+Explanation:
+
+The strings s and t can not be made identical as 'o' needs to be mapped to both 'a' and 'r'.
+
+Example 3:
+
+Input: s = "paper", t = "title"
+
+Output: true
+
+Constraints:
+
+1 <= s.length <= 5 * 104
+t.length == s.length
+s and t consist of any valid ascii character.*/
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+
+       int chsize = s.size();
+      
+      map<char,char> mpp1;
+      map<char,char> mpp2;
+
+      for(int i=0;i<chsize;i++)
+      {
+          char ch1=s[i];
+          char ch2=t[i];
+
+          if(mpp1.find(ch1) != mpp1.end() && mpp1[ch1] != ch2 || mpp2.find(ch2) != mpp2.end() && mpp2[ch2] != ch1)
+          {
+            return false;
+          }
+
+          mpp1[ch1] = ch2;
+          mpp2[ch2] = ch1;
+      }
+
+       return true;
+
+    }
+};
